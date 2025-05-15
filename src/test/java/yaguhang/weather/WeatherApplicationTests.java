535b3394b860735069872077d09a3bde64ec9654
Minseok Choi <gd10080008@gmail.com>
2025-05-15 16:49:45 +0900

    Initial Commit

---
+++ src/test/java/yaguhang/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package yaguhang.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
